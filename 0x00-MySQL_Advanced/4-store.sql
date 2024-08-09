cdf289f4a434cbc0aea492cc40acc6869db6f73f
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-08-09 02:02:06 +0100

    Added task 3&4

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,11 @@
+-- Write a SQL script that creates a trigger that decreases the quantity
+-- of an item after adding a new order
+DELIMITER $$
+CREATE DELIMITER decrease_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+    UPDATE items
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
+END $$;
+DELIMITER;
