49fcd492190ec40d6a5862acef00c5a451ab39ab
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-08-09 02:51:59 +0100

    Added task 6

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,23 +1,25 @@
 -- Write a SQL script that creates a stored procedure AddBonus
 -- should add a new correction for a student
 CREATE PROCEDURE AddBonus (
-    user_id INTEGER
-    project_name VARCHAR(255)
-    SCORE INTEGER
-)
+    user_id INT, 
+    project_name VARCHAR(255), 
+    score FLOAT)
+
 BEGIN
-    DECLARE project_id INT;
+    DECLARE project_count INT DEFAULT 0;
+    DECLARE project_id INT DEFAULT 0;
 
-    SELECT id INTO project_id
-    FROM projects
-    WHERE name = project_name
-
-IF project_id IS NULL THEN
-    INSERT INTO projects (name)
-    VALUES (project_name);
-    SET project_id = LAST_INSERT_ID();
-END IF;
-
-INSERT INTO corrections (user_id, project_id, score)
-VALUES (user_id, project_id, score);
-END;
+    SELECT COUNT(id)
+        INTO project_count
+        FROM projects
+        WHERE name = project_name;
+    IF project_count = 0 THEN
+        INSERT INTO projects(name)
+            VALUES(project_name);
+    END IF;
+    SELECT id
+        INTO project_id
+        FROM projects
+        WHERE name = project_name;
+    INSERT INTO corrections(user_id, project_id, score)
+        VALUES (user_id, project_id, score);
