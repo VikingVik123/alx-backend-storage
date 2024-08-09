1c969a742a2a139f0125c7c9c88547c96242ae02
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-08-09 02:15:30 +0100

    Added task 3&4

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -5,6 +5,7 @@ DELIMITER $$
 CREATE DELIMITER decrease_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
+BEGIN
     UPDATE items
         SET quantity = quantity - NEW.number
         WHERE name = NEW.item_name;
